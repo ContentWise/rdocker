9f775dce5a2f0a2c360cceeb9e1aac1db34c772f
Marco Miglierina <marco.miglierina@contentwise.tv>
2018-04-11 17:18:10 +0200

    export also user home, id and group id

--- rdocker.sh
+++ rdocker.sh
@@ -150,6 +150,9 @@ if [[ $line == $success_msg ]]; then
         echo -e "Press ${RED}Ctrl+D${NC} to stop forwarding and exit the bash session."
         export DOCKER_MACHINE_HOSTNAME="${remote_host}"
         export DOCKER_MACHINE_SSH_AUTH_SOCK=$(ssh -o ControlPath="$control_path" $remote_host echo \$SSH_AUTH_SOCK)
+        export DOCKER_MACHINE_USER_HOME=$(ssh -o ControlPath="$control_path" $remote_host echo \$HOME)
+        export DOCKER_MACHINE_USER_ID=$(ssh -o ControlPath="$control_path" $remote_host id -u)
+        export DOCKER_MACHINE_GROUP_ID=$(ssh -o ControlPath="$control_path" $remote_host id -g)
         bash -l
     fi
 
