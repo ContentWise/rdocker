04eb309c5177f1ad2415f00e8520981a5d71abe4
Marco Miglierina <marco.miglierina@icloud.com>
2018-05-09 15:00:15 +0200

    Update rdocker.sh

--- rdocker.sh
+++ rdocker.sh
@@ -154,7 +154,7 @@ if [[ $line == $success_msg ]]; then
         export DOCKER_MACHINE_WORKSPACE=${DOCKER_MACHINE_USER_HOME}/workspace
         export DOCKER_MACHINE_USER_ID=$(ssh -o ControlPath="$control_path" $remote_host id -u)
         export DOCKER_MACHINE_GROUP_ID=$(ssh -o ControlPath="$control_path" $remote_host id -g)
-        bash -l
+        $SHELL
     fi
 
     kill -15 $CONNECTION_PID
